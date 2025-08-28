bc49738c32956fe835e3ab3b6079f958e6423948
me-lucas-al <me.lucasalmeida@gmail.com>
2025-08-28 12:33:02 -0300

    fix: add justify-items-center and mx-auto to CardList layout for improved alignment

--- src/components/Card-List.tsx
+++ src/components/Card-List.tsx
@@ -4,11 +4,11 @@ import { CardDemo } from "./Card";
 export default function CardList() {
   const { data: characters } = useGetAllCharacters();
 
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full h-full gap-6">
+    return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 justify-items-center mx-auto w-full h-full">
       {characters?.map((character: ICharacter) => (
         <CardDemo key={character.id} character={character} />
       ))}
     </div>
-  );
+    );
 }
